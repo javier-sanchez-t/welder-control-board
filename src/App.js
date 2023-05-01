762955343a23c2d2e86460eeebbbe1b7c997eaa2
javier-sanchez-t <jefe10jav@gmail.com>
2023-05-01 13:21:40 -0600

    feat: add code editor

--- src/App.js
+++ src/App.js
@@ -1,30 +1,80 @@
-import logo from "./logo.svg";
-import "./App.css";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/ext-language_tools";
 import Row from "./components/row";
 import Button from "./components/button";
 import Component from "./components/component";
+import { useCallback, useState } from "react";
+
+import styles from "./styles.module.css";
 
 function App() {
-  const board = [
+  const CODE_EDITOR_ID = "code-editor";
+  const InitialBoard = [
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
   ];
+  const [board, setBoard] = useState(InitialBoard);
+  const [counter, setCounter] = useState(0);
+  const [code, setCode] = useState("");
+
+  const onItemDrop = useCallback(
+    (rowIndex, columnIndex) => {
+      const newCounter = counter + 1;
+      setCounter(newCounter);
+      const newBoard = board;
+      newBoard[rowIndex][columnIndex] = newCounter;
+      setBoard(newBoard);
+
+      const items = newBoard
+        .flat()
+        .sort((a, b) => a - b)
+        .filter(Boolean);
+      let newCode = "";
+      items.forEach((item) => {
+        newCode += `Instrucción ${item} \n`;
+      });
+      setCode(newCode);
+    },
+    [board, counter]
+  );
 
   return (
     <div className="App">
-      <Component />
-      {board.map((row, rowIndex) => {
-        return (
-          <Row key={"row_" + rowIndex}>
-            {row.map((column, columnIndex) => {
-              return <Button key={"button_" + columnIndex} />;
-            })}
-          </Row>
-        );
-      })}
+      <div className={styles["app-container"]}>
+        <div>
+          <Component />
+          {board.map((row, rowIndex) => {
+            return (
+              <Row key={"row_" + rowIndex}>
+                {row.map((column, columnIndex) => {
+                  return (
+                    <Button
+                      key={"button_" + columnIndex}
+                      index={board[rowIndex][columnIndex]}
+                      onDropInto={() => onItemDrop(rowIndex, columnIndex)}
+                    />
+                  );
+                })}
+              </Row>
+            );
+          })}
+        </div>
+
+        <div>
+          <AceEditor
+            mode="javascript"
+            theme="monokai"
+            value={code}
+            name={CODE_EDITOR_ID}
+            editorProps={{ $blockScrolling: true }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,20 +1,31 @@
 import { useState } from "react";
 import styles from "./styles.module.css";
 
-export default function Button() {
+export default function Button({ index, onDropInto }) {
   const [active, setActive] = useState(false);
   const onDrop = () => {
     console.log("OnDROP");
     setActive(true);
+    onDropInto();
   };
 
   const allowDrop = (ev) => {
     ev.preventDefault();
   };
 
+  const handleClick = () => {
+    setActive(false);
+  };
+
   return (
     <div className={styles.button} onDrop={onDrop} onDragOver={allowDrop}>
-      <input type="radio" checked={active} onChange={() => {}} />
+      {index}
+      <input
+        type="radio"
+        checked={active}
+        onChange={() => {}}
+        onClick={handleClick}
+      />
     </div>
   );
 }
