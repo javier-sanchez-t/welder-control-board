ad10625db2796ae9650aea76936fac3e2ef74171
pethersex <132873694+pethersex@users.noreply.github.com>
2023-05-08 00:31:33 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
             let newCode = "#include <VarSpeedServo.h> \n\nVarSpeedServo myservo1;    // create servo object to control a servo \nVarSpeedServo myservo2; \n\nvoid setup() { \nmyservo1.attach(8);  // attaches the servo on pin 9 to the servo object \nmyservo2.attach(9);  // attaches the servo on pin 9 to the servo object \n} \n\nvoid loop() {\n";
             items.forEach((item) => {
                 if (item === 1) {
-                    newCode += "    myservo1.write(60, 10, true);     // pierna abajo \n    myservo2.write(100, 10, true);     // pierna abajo \n   delay(5000); \n\n   myservo1.write(70, 10, true);     // pierna abajo \n    myservo2.write(130, 10, true);     // pierna abajo \n   delay(5000); ";
+                    newCode += "myservo1.write(60, 10, true);     // pierna abajo \nmyservo2.write(100, 10, true);     // pierna abajo \ndelay(5000); \n\nmyservo1.write(70, 10, true);     // pierna abajo \nmyservo2.write(130, 10, true);     // pierna abajo \ndelay(5000); ";
                 }
             });
             newCode+="\n\n}";
