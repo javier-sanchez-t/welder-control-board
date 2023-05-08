93d36158e3df3c6b465fc59221667014366cd7c0
pethersex <132873694+pethersex@users.noreply.github.com>
2023-05-08 00:12:52 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,74 +10,77 @@ import { useCallback, useState } from "react";
 import styles from "./styles.module.css";
 
 function App() {
-  const CODE_EDITOR_ID = "code-editor";
-  const InitialBoard = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-  ];
-  const [board, setBoard] = useState(InitialBoard);
-  const [counter, setCounter] = useState(0);
-  const [code, setCode] = useState("");
+    const CODE_EDITOR_ID = "code-editor";
+    const InitialBoard = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+    const [board, setBoard] = useState(InitialBoard);
+    const [counter, setCounter] = useState(0);
+    const [code, setCode] = useState("");
 
-  const onItemDrop = useCallback(
-    (rowIndex, columnIndex) => {
-      const newCounter = counter + 1;
-      setCounter(newCounter);
-      const newBoard = board;
-      newBoard[rowIndex][columnIndex] = newCounter;
-      setBoard(newBoard);
+    const onItemDrop = useCallback(
+        (rowIndex, columnIndex) => {
+            const newCounter = counter + 1;
+            setCounter(newCounter);
+            const newBoard = board;
+            newBoard[rowIndex][columnIndex] = newCounter;
+            setBoard(newBoard);
 
-      const items = newBoard
-        .flat()
-        .sort((a, b) => a - b)
-        .filter(Boolean);
-      let newCode = "";
-      items.forEach((item) => {
-        newCode += `Instrucción ${item} \n`;
-      });
-      setCode(newCode);
-    },
-    [board, counter]
-  );
+            const items = newBoard
+                .flat()
+                .sort((a, b) => a - b)
+                .filter(Boolean);
+            let newCode = "#include <VarSpeedServo.h> \n\n VarSpeedServo myservo1;    // create servo object to control a servo \n\nVarSpeedServo myservo2; \n\n void setup() { \n\n myservo1.attach(8);  // attaches the servo on pin 9 to the servo object \n\nmyservo2.attach(9);  // attaches the servo on pin 9 to the servo object \n } \nvoid loop() {";
+            items.forEach((item) => {
+                if (item === 1) {
+                    newCode += "  myservo1.write(60, 10, true);     // pierna abajo \n myservo2.write(100, 10, true);     // pierna abajo \n delay(5000); \n\nmyservo1.write(70, 10, true);     // pierna abajo \nmyservo2.write(130, 10, true);     // pierna abajo \n delay(5000); ";
+                }
+            });
+            newCode+="}";
+            setCode(newCode);
+        },
+        [board, counter]
+    );
 
-  return (
-    <div className="App">
-      <div className={styles["app-container"]}>
-        <div>
-          <Component />
-          {board.map((row, rowIndex) => {
-            return (
-              <Row key={"row_" + rowIndex}>
-                {row.map((column, columnIndex) => {
-                  return (
-                    <Button
-                      key={"button_" + columnIndex}
-                      index={board[rowIndex][columnIndex]}
-                      onDropInto={() => onItemDrop(rowIndex, columnIndex)}
+    return (
+        <div className="App">
+            <div className={styles["app-container"]}>
+                <div>
+                    <Component />
+                    {board.map((row, rowIndex) => {
+                        return (
+                            <Row key={"row_" + rowIndex}>
+                                {row.map((column, columnIndex) => {
+                                    return (
+                                        <Button
+                                            key={"button_" + columnIndex}
+                                            index={board[rowIndex][columnIndex]}
+                                            onDropInto={() => onItemDrop(rowIndex, columnIndex)}
+                                        />
+                                    );
+                                })}
+                            </Row>
+                        );
+                    })}
+                </div>
+
+                <div>
+                    <AceEditor
+                        mode="javascript"
+                        theme="monokai"
+                        fontSize={18}
+                        value={code}
+                        name={CODE_EDITOR_ID}
+                        editorProps={{ $blockScrolling: true }}
                     />
-                  );
-                })}
-              </Row>
-            );
-          })}
+                </div>
+            </div>
         </div>
-
-        <div>
-          <AceEditor
-            mode="javascript"
-            theme="monokai"
-            fontSize={18}
-            value={code}
-            name={CODE_EDITOR_ID}
-            editorProps={{ $blockScrolling: true }}
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
