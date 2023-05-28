3a396407d51d36410d533e1819dbc145f8874204
javier-sanchez-t <jefe10jav@gmail.com>
2023-05-28 01:58:36 -0600

    adding missing ;

--- src/App.js
+++ src/App.js
@@ -117,7 +117,7 @@ function App() {
             myservo1.write(140, 30, true);    
             delay(6000);
             myservo1.write(90, 30, true);    
-            myservo2.write(90, 30, true)\n`;
+            myservo2.write(90, 30, true);\n`;
         instructions += `
             // Regresar a punto de partida`;
         instructions += moveServoInstructions(point, "+");
